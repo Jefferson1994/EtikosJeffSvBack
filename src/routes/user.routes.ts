30f2072f6fbd96c9dcd7689f230bc74f15b622d0
Jefferson1994 <jeffersonk132008@hotmail.com>
2025-10-16 17:00:16 -0500

    limitador de peticiones al login

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import {  UserController } from "../controllers/UserController";
 import { authenticateJWT } from '../middlewares/auth.middleware'; 
+import rateLimit from "express-rate-limit";
 
 const router = Router();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutos
+    max: 5,
+    message: { ok: false, mensaje: 'Demasiadas solicitudes, por favor intente de nuevo más tarde.' },
+    standardHeaders: true,
+    legacyHeaders: false,
+    skipSuccessfulRequests: true,
+});
+
 router.post("/users", UserController.crear);
-router.post("/login", UserController.LoginPorMail);
+router.post("/login",limiter, UserController.LoginPorMail);
 router.post("/rol", UserController.RolesActivos);
 router.post('/buscar-por-cedula', authenticateJWT, UserController.obtenerPorCedula);
 router.post('/empresasVinculadas', authenticateJWT, UserController.obtenerMisNegociosVinculados);
